c5e2ff5c9de0c651fd9c3563f1db64e51ba5d8d6
iamgus123 <102215701+iamgus123@users.noreply.github.com>
2022-12-01 01:27:18 +0900

    Add files via upload

---
+++ src/javaapplication1/RelationalCalsses/UserFactory.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1.RelationalCalsses;
+
+
+/**
+ *
+ * @author Peter
+ */
+public class UserFactory {
+    
+    private static UserFactory instance; 
+    
+    private UserFactory()
+    {
+        
+    }
+      public static UserFactory getInstance ()
+    {
+        if( instance == null)
+        {
+            instance = new UserFactory();
+        }
+        return instance;
+    }
+
+    public  String student = "Students";
+    public  String teacher = "Professor";
+    public  String admin = "admin";
+
+    public  User usercreation(String Usertybe) {
+        switch (Usertybe) {
+            case "Students":
+                return new Student();
+            case "Professor":
+                return new Professor();
+            case "admin":
+                return new Admin();
+            default:
+                return null;
+
+        }
+    }
+
+}
